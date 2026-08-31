c4696bf7c37208eeae76161464bbbe00ddc49760
Adam Ghanem <adam.ghanem.it@gmail.com>
2026-08-31 04:32:44 +0100

    style: load production redesign

--- web/client/src/main.tsx
+++ web/client/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./production-mobile.css";
+import "./production-redesign.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
