e06894ccf19f371cd178afceabacd593a774e5db
Adam Ghanem <adam.ghanem.it@gmail.com>
2026-09-15 08:47:21 +0100

    style(web): load Analyze premium polish

--- web/client/src/main.tsx
+++ web/client/src/main.tsx
@@ -12,6 +12,7 @@ import "./mobile-navigation.css";
 import "./progress-mobile-nav.css";
 import "./home-play-polish.css";
 import "./play-premium-polish.css";
+import "./analyze-premium-polish.css";
 import "./playMoveListFollow";
 
 createRoot(document.getElementById("root")!).render(<App />);
